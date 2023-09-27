5a7f8541984c46ad0e61a8a43a53fc8d8aba3b33
Restyled.io <commits@restyled.io>
2023-09-27 22:10:27 +0000

    Restyled by autopep8

--- Driver.py
+++ Driver.py
@@ -5,7 +5,8 @@ if __name__ == "__main__":
     prevPrice = 48.12
     interest = 1.0
     oleoConstant = 0.8
-    print(f"Calculating theoretical value for previous price {prevPrice}, interest {interest}, Oleo Constant {oleoConstant}")
+    print(
+        f"Calculating theoretical value for previous price {prevPrice}, interest {interest}, Oleo Constant {oleoConstant}")
 
     util = PricingUtil.PricingUtil()
     val = util.calcVal(prevPrice, interest, oleoConstant)
