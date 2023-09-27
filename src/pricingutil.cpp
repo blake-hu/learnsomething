5d7aca1c6bc2721f1210d9635903bc2ed349c864
Blake Hu <64202537+blake-hu@users.noreply.github.com>
2023-09-27 17:05:28 -0500

    Create PricingUtil C++ module and write unit tests

--- src/pricingutil.cpp
+++ src/pricingutil.cpp
@@ -1,1 +1,13 @@
 #include "pricingutil.h"
+
+PricingUtil::PricingUtil() {};
+
+float PricingUtil::calcVal(float prevPrice, float interest, float oleoConstant) {
+    float newVal = prevPrice * (0.9 + interest) * oleoConstant;
+    this->val = newVal;
+    return newVal;
+}
+
+float PricingUtil::getVal() {
+    return this->val;
+}
