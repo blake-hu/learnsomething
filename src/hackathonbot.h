388d08f11806b5d0a574e047871bb3396cf03b44
Restyled.io <commits@restyled.io>
2023-09-28 00:21:37 +0000

    Restyled by clang-format

--- src/hackathonbot.h
+++ src/hackathonbot.h
@@ -2,26 +2,27 @@
 // Created by Ethan on 9/13/2023.
 //
 
-#include <deque>
 #include "action.h"
+#include <deque>
 
 #ifndef LEARNSOMETHING_HACKATHONBOT_H
 #define LEARNSOMETHING_HACKATHONBOT_H
 
 class HackathonBot {
 public:
-    HackathonBot();
-    void takeAction(float price);
-    double getBalance();
-    bool isHolding();
+  HackathonBot();
+  void takeAction(float price);
+  double getBalance();
+  bool isHolding();
+
 private:
-    double balance;
-    bool holding;
-    std::deque<float> prevPrices;
-    int dropStreak = 0;
-    int riseStreak = 0;
-    void buy();
-    void sell();
+  double balance;
+  bool holding;
+  std::deque<float> prevPrices;
+  int dropStreak = 0;
+  int riseStreak = 0;
+  void buy();
+  void sell();
 };
 
-#endif //LEARNSOMETHING_HACKATHONBOT_H
+#endif // LEARNSOMETHING_HACKATHONBOT_H
