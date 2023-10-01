5520051c84f7110598a9c74e13ae92227315570e
Restyled.io <commits@restyled.io>
2023-10-01 07:55:12 +0000

    Restyled by pyment

--- prime.py
+++ prime.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 
 
 def disasterCode():
+    """ """
     # first find all primes up to sqrt(2500)
     primes = [2]
     for n in range(3, round(math.sqrt(1000)) + 1, 2):
